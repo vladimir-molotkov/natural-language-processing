a166afe403acb0f650c2d6971f7c55899826d448
vladimir-molotkov <molotkov1vl@gmail.com>
2025-03-30 12:06:48 +0300

    add solved homework

--- homeworks/hw02_attention/template_p01.py
+++ homeworks/hw02_attention/template_p01.py
@@ -14,29 +14,33 @@ def softmax(vector):
     return softmax_
 
 def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
-    '''
+    """
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     W_mult: np.array of shape (n_features_dec, n_features_enc)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
-    '''
-    # your code here
-    
+    """
+    attention_scores = decoder_hidden_state.T @ W_mult @ encoder_hidden_states
+    attention_weigths = softmax(attention_scores) 
+    attention_vector = attention_weigths.dot(encoder_hidden_states.T).T
     return attention_vector
 
-def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
-    '''
+def additive_attention(
+    decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec
+):
+    """
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     v_add: np.array of shape (n_features_int, 1)
     W_add_enc: np.array of shape (n_features_int, n_features_enc)
     W_add_dec: np.array of shape (n_features_int, n_features_dec)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
-    '''
-    # your code here
-    
+    """
+    attention_scores = v_add.T @ np.tanh(W_add_enc @ encoder_hidden_states + W_add_dec @ decoder_hidden_state)
+    attention_weigths = softmax(attention_scores) 
+    attention_vector = attention_weigths.dot(encoder_hidden_states.T).T
     return attention_vector
